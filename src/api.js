269cd149601137def45682c2b1af7c7da595a312
Eren Guldas <erenguldas@gmail.com>
2020-06-23 09:02:16 +0300

    https

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const weatherApiOptions = {
-  baseURL: 'http://api.openweathermap.org/data/2.5',
+  baseURL: 'https://api.openweathermap.org/data/2.5',
   headers: {
     'Content-Type': 'application/json',
   },
